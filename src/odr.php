b27254841ef7c7442ada0e7dc2bd903bf3cc4a2a
staticall <staticall@gmail.com>
2017-03-27 20:21:24 +0700

    Removed debug URL

--- src/odr.php
+++ src/odr.php
@@ -694,7 +694,7 @@ class Odr_Whmcs
     const STATUS_SUCCESS = 'success';
     const STATUS_ERROR   = 'error';
 
-    const URL_TEST = 'http://apiodr.kaa.tomoko.rosapp.ru';
+    const URL_TEST = 'http://api.odrregistry.nl';
     const URL_LIVE = Api_Odr::URL;
 
     const MODULE = 'odr';
